acbe3752274bc131d533f69be65f409aba247265
Barionetta <kasia-matuszek@o2.pl>
2024-01-18 13:06:02 +0100

    some kind of front

---
+++ app/db.py
@@ -0,0 +1,163 @@
+import psycopg2
+from flask import current_app
+
+def connect_to_db():
+    try:
+        conn = psycopg2.connect(
+            user = current_app.config['DATABASE_USER'],
+            password = current_app.config['DATABASE_PASSWORD'],
+            dbname = current_app.config['DATABASE_NAME'],
+            host= current_app.config['DATABASE_HOST'],
+            port= current_app.config['DATABASE_PORT']
+        )
+        print("Database connected successfully")
+        return conn
+    except Exception:
+        print("Database not connected successfully")
+
+def connect_to_db_as_user():
+    try:
+        conn = psycopg2.connect(
+            user = current_app.config['DATABASE_USER_U'],
+            password = current_app.config['DATABASE_PASSWORD_U'],
+            dbname = current_app.config['DATABASE_NAME'],
+            host= current_app.config['DATABASE_HOST'],
+            port= current_app.config['DATABASE_PORT']
+        )
+        print("Database connected successfully")
+        return conn
+    except Exception:
+        print("Database not connected successfully")
+
+def connect_to_db_as_admin():
+    try:
+        conn = psycopg2.connect(
+            user = current_app.config['DATABASE_USER_A'],
+            password = current_app.config['DATABASE_PASSWORD_A'],
+            dbname = current_app.config['DATABASE_NAME'],
+            host= current_app.config['DATABASE_HOST'],
+            port= current_app.config['DATABASE_PORT']
+        )
+        print("Database connected successfully")
+        return conn
+    except Exception:
+        print("Database not connected successfully")
+
+class movie:
+    def __init__(self, title, tier, genre):
+        self.title = title
+        self.tier = tier
+        self.genre = genre
+
+
+def get_ALL_data_about_movies(sort):
+    conn = connect_to_db()
+    cur = conn.cursor()
+
+    match sort:
+        case 1:
+            cur.execute("""
+                SELECT Mo.movie_title, Ge.genre_name, acc_t.account_type
+                FROM movies Mo
+                INNER JOIN account_types acc_t ON Mo.account_type_id = acc_t.account_type_id 
+                INNER JOIN genres Ge ON Mo.genre_id = Ge.genre_id
+                ORDER BY Mo.account_type_id;
+                """)
+        case 2:
+            cur.execute("""
+                 SELECT Mo.movie_title, Ge.genre_name, acc_t.account_type
+                 FROM movies Mo
+                 INNER JOIN account_types acc_t ON Mo.account_type_id = acc_t.account_type_id 
+                 INNER JOIN genres Ge ON Mo.genre_id = Ge.genre_id
+                 ORDER BY Mo.movie_title;
+                 """)
+        case 3:
+            cur.execute("""
+                SELECT Mo.movie_title, Ge.genre_name, acc_t.account_type
+                FROM movies Mo
+                INNER JOIN account_types acc_t ON Mo.account_type_id = acc_t.account_type_id 
+                INNER JOIN genres Ge ON Mo.genre_id = Ge.genre_id
+                ORDER BY Ge.genre_name;
+                """)
+
+    rows = cur.fetchall()
+
+    returned_movies = []
+    for data in rows:
+        mov = movie(title=str(data[0]), genre=str(data[1]), tier=str(data[2]))
+        returned_movies.append(mov)
+
+    print('ALL Data fetched successfully, total rows: ', len(returned_movies))
+    return returned_movies
+
+# TODO do modyfikacji
+def get_data_about_movies(tier=None):
+    conn = connect_to_db()
+    cur = conn.cursor()
+
+    if tier is None or tier == 4:
+        cur.execute("""
+                SELECT Mo.movie_title, Ge.genre_name, acc_t.account_type
+                FROM movies Mo
+                INNER JOIN account_types acc_t ON Mo.account_type_id = acc_t.account_type_id 
+                INNER JOIN genres Ge ON Mo.genre_id = Ge.genre_id
+                ORDER BY Mo.account_type_id;
+                """)
+    elif tier == 3:
+        cur.execute("""
+                SELECT Mo.movie_title, Ge.genre_name, acc_t.account_type
+                FROM movies Mo
+                INNER JOIN account_types acc_t ON Mo.account_type_id = acc_t.account_type_id 
+                INNER JOIN genres Ge ON Mo.genre_id = Ge.genre_id 
+                WHERE Mo.account_type_id < 4
+                ORDER BY Mo.account_type_id;
+                """)
+    elif tier == 2:
+        cur.execute("""
+                SELECT Mo.movie_title, Ge.genre_name, acc_t.account_type
+                FROM movies Mo
+                INNER JOIN account_types acc_t ON Mo.account_type_id = acc_t.account_type_id 
+                INNER JOIN genres Ge ON Mo.genre_id = Ge.genre_id 
+                WHERE Mo.account_type_id = 2
+                ORDER BY Mo.account_type_id;
+                """)
+    else:
+        print("Invalid 'tier' value:", tier)
+
+    rows = cur.fetchall()
+
+    returned_movies = []
+    for data in rows:
+        mov = movie(title=str(data[0]), genre=str(data[1]), tier=str(data[2]))
+        returned_movies.append(mov)
+
+    print('Data fetched successfully, total rows: ', len(returned_movies))
+    return returned_movies
+
+
+
+class Users_data:
+    def __init__(self, id, nick, tier):
+        self.tier = tier
+        self.nick = nick
+        self.id = id
+
+
+def get_data_about_users():
+    conn = connect_to_db()
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute(f""" SELECT ACC.account_id, ACC.nick, acc_t.account_type, ACC.account_type_id
+                                FROM accounts ACC
+                                INNER JOIN account_types acc_t
+                                ON ACC.account_type_id = acc_t.account_type_id
+                                order by acc_t.account_type_id, ACC.account_id ; """)
+            rows = cursor.fetchall()
+
+    returned_users = []
+    for data in rows:
+        data = Users_data(id=int(data[0]), nick=str(data[1]), tier=str(data[2]))
+        returned_users.append(data)
+
+    print('Data fetched successfully, total rows: ', len(returned_users))
+    return returned_users
---
+++ app/other_routes.py
@@ -0,0 +1,38 @@
+from flask import render_template, request
+from flask import Blueprint, render_template
+from app.db import get_data_about_movies, get_ALL_data_about_movies
+
+other_bp = Blueprint('other_routes', __name__)
+
+
+@other_bp.route('/')  # domyślna strona startowa strona
+def index():
+    print('Started')
+    return render_template("other_routes/index.html")
+
+
+@other_bp.route("/authors")
+def authors():
+    return render_template("other_routes/authors.html")
+
+
+@other_bp.route("/movies", methods=["POST", "GET"])
+def movies():
+    if request.method == 'POST':
+        sort_type = int(request.form.get('sort'))
+        movies_list = get_ALL_data_about_movies(sort=sort_type)
+        return render_template("other_routes/Movies.html", movies=movies_list)
+    else:
+        movies_list = get_data_about_movies()
+        return render_template("other_routes/Movies.html", movies=movies_list)
+
+"""
+@other_bp.route("/prices")
+def prices():
+    return render_template("other_routes/Prices.html")
+
+
+@other_bp.route("/coming_soon")
+def coming_soon():
+    return render_template("other_routes/Coming_soon.html")
+"""
